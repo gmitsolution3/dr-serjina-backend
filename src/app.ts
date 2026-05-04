b35713d5cdbf79dbd9262783cd5aeb8cdd7d629c
gmitsolution3 <gmitsolution3@gmail.com>
2026-05-04 17:52:25 +0600

    feat: domain added on cors list

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,29 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 
 import errorHandler from "./middlewares/error.middleware";
 import router from "./routes";
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:3000", "https://www.drskserjina.com"];
+
 // global middleware configuration
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow server-to-server / Postman
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // handle routes
